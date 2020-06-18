4d1c1a5e54deb790a7e6899eca30f6a70a585f7d
Rohmad <rohmad37@ymail.com>
2020-06-18 07:15:05 +0700

    'Revisi Pagi'

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let sidebar = [["Home"], ["Setting"], ["Logout"], ["Profile"]];
 
-for (var i = 0, l1 = sidebar.length; i < l1; i++) {
-  for (var j = 0, l2 = sidebar[i].length; j < l2; j++) {
+let sidebarHeader = document.getElementById("show");
+
+for (let i = 0, l1 = sidebar.length; i < l1; i++) {
+  for (let j = 0, l2 = sidebar[i].length; j < l2; j++) {
     let newP = document.createElement("p");
     let teks = document.createTextNode(sidebar[i][j]);
 
     newP.appendChild(teks);
+    sidebarHeader.appendChild(newP);
 
     const article = document.getElementById("show");
     article.appendChild(newP);
   }
+  let hr = document.createElement("hr");
+  sidebarHeader.appendChild(hr);
 }
