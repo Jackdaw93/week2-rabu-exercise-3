e23b728d55514b3c8ebfe354b09dd1e0230c38ef
Rohmad <rohmad37@ymail.com>
2020-06-18 07:01:48 +0700

    'Minus Undeline'

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-let sidebar = [["Home Profile"], ["Setting"], ["Logout"], ["Test"]];
+let sidebar = [["Home"], ["Setting"], ["Logout"], ["Profile"]];
 
 for (var i = 0, l1 = sidebar.length; i < l1; i++) {
   for (var j = 0, l2 = sidebar[i].length; j < l2; j++) {
     let newP = document.createElement("p");
-    let newH = document.createElement("hr");
     let teks = document.createTextNode(sidebar[i][j]);
-    newH.appendChild(newP);
+
     newP.appendChild(teks);
 
     const article = document.getElementById("show");
-    article.appendChild(newH);
+    article.appendChild(newP);
   }
 }
